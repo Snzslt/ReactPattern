80d65a3d5457fcdf3b8a8b011465de2c38e2d2be
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-05-15 13:32:35 -0400

    sharing cross component state when working with compounded component

--- .history/src/components/Accordion/AccordionTitle_20240515131803.jsx
+++ .history/src/components/Accordion/AccordionTitle_20240515131803.jsx
@@ -1,9 +1,7 @@
 import { useAccordionContext } from './Accordion.jsx';
-import { useAccordionItemContext } from './AccordionItem.jsx';
 
-export default function AccordionTitle({ className, children }) {
+export default function AccordionTitle({ id, className, children }) {
   const { toggleItem } = useAccordionContext();
-  const id = useAccordionItemContext();
   return (
     <h3 className={className} onClick={() => toggleItem(id)}>
       {children}
